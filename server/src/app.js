671854b55aac753b023d68b66ebc9b119575e5aa
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2021-02-04 15:19:53 +0530

    :hammer: fixed small error

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import morgan from 'morgan';
 import { urlencoded, json } from 'body-parser';
 import cors from 'cors';
 
 const app = express();
-
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+ 
+app.use(morgan('combined', { stream: accessLogStream }));
 app.use(cors());
 app.use(urlencoded({ extended: false }));
 app.use(json());
