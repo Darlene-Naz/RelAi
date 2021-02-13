94b28f08873c939fd5a1ed01fbce767bd53f9837
Darlene Nazareth <46684660+Darlene-Naz@users.noreply.github.com>
2021-02-13 11:53:53 +0530

    :sparkles: analyse & summarize endpoints connected + highlighting done + dynamic component update done

--- client/src/views/widgets/WidgetTextEditor.js
+++ client/src/views/widgets/WidgetTextEditor.js
@@ -1,12 +1,36 @@
 import CIcon from '@coreui/icons-react';
-import { CCard, CFade, CCardBody, CCardFooter, CCardHeader, CLink, CTextarea, CCol, CButton, CButtonGroup, CRow, CCollapse } from '@coreui/react';
+import axios from 'axios'
+import Highlighter from "react-highlight-words";
+import { CCard, CFade, CCardBody, CCardFooter, CCardHeader, CLink, CTextarea, CCol, CButton, CButtonGroup, CRow, CCollapse, CDropdown, CDropdownToggle, CDropdownMenu, CDropdownItem } from '@coreui/react';
 import React, { useState } from 'react';
+
 function WidgetTextEditor(props) {
     const [ collapsed, setCollapsed ] = useState(true)
     const [ showCard, setShowCard ] = useState(true)
-
+    const [ changedData, updateData ] = useState(props.text)
+    const [ analysedData, showAnalysis ] = useState({
+        showSummary: false
+    })
+    function showUpdatedData(e) {
+        const { value } = e.target;
+        updateData(value)
+        showAnalysis({ showSummary: false })
+    }
+    function onSummarize(event) {
+        showAnalysis({ ...analysedData, showSummary: true })
+    }
+    function onAnalyze(event) {
+        if (changedData === "") updateData(props.text);
+        axios.post('http://localhost:8000/api/v1/text/analyze', { text: changedData === "" ? props.text : changedData }, { headers: { 'content-type': 'application/json' } })
+            .then(res => {
+                console.log(res.data);
+                showAnalysis({ ...analysedData, message: res.data.message })
+            })
+            .catch(e => console.log(e))
+        event.preventDefault()
+    }
     return (
-        !props.hide && <CFade in={showCard}>
+        <CFade in={showCard}>
             <CCard>
                 <CCardHeader>
                     <CRow>
@@ -16,52 +40,66 @@ function WidgetTextEditor(props) {
                         </CCol>
                         <CCol sm="9" className="d-none d-md-block">
                             <div className="card-header-actions float-right">
-                                <CLink className="card-header-action">
-                                    <CIcon name="cil-settings" />
-                                </CLink>
                                 <CLink className="card-header-action" onClick={() => setCollapsed(!collapsed)}>
                                     <CIcon name={collapsed ? 'cil-chevron-bottom' : 'cil-chevron-top'} />
                                 </CLink>
-                                <CLink className="card-header-action" onClick={() => setShowCard(false)}>
+                                <CLink className="card-header-action" onClick={() => setShowCard(true)}>
                                     <CIcon name="cil-x-circle" />
                                 </CLink>
                             </div>
-                            <CButton color="primary" className="float-right mr-3">
-                                <CIcon name="cil-cloud-download" />
-                            </CButton>
+                            <CDropdown>
+                                <CDropdownToggle caret={false} color="primary" className="float-right mr-3">
+                                    <CIcon className="my-0" name="cil-cloud-download" />
+                                </CDropdownToggle>
+                                <CDropdownMenu className="pt-0" placement="bottom-end">
+                                    <CDropdownItem>PDF</CDropdownItem>
+                                    <CDropdownItem>.txt</CDropdownItem>
+                                    <CDropdownItem>.doc</CDropdownItem>
+                                </CDropdownMenu>
+                            </CDropdown>
                             <CButtonGroup className="float-right mr-3">
                                 {
-                                    [ 'Day', 'Month', 'Year' ].map(value => (
+                                    [ 'Lemmas', 'Phrases', 'Sentences' ].map(value => (
                                         <CButton
                                             color="outline-secondary"
                                             key={value}
                                             className="mx-0"
-                                            active={value === 'Month'}
+                                            active={value === 'Phrases'}
                                         >
                                             {value}
                                         </CButton>
                                     ))
                                 }
                             </CButtonGroup>
-
+                            <CButton shape="pill" color="danger" className="float-right mr-3 text-center" onClick={onSummarize}>Summarize</CButton>
+                            <CButton shape="pill" color="info" className="float-right mr-3 text-center" onClick={onAnalyze} >Analyze</CButton>
                         </CCol>
                     </CRow>
-
-
                 </CCardHeader>
                 <CCollapse show={collapsed}>
                     <CCardBody>
-                        <CTextarea
-                            name="editorText"
-                            id="editorText"
-                            rows="2"
-                            placeholder="Content..."
-                            value={props.text}
-                        />
+                        {
+                            (!analysedData.showSummary) ?
+                                <CTextarea
+                                    name="editorText"
+                                    id="editorText"
+                                    rows="9"
+                                    placeholder="Content..."
+                                    value={changedData}
+                                    onChange={showUpdatedData}
+                                /> :
+                                <Highlighter
+                                    highlightClassName="YourHighlightClass"
+                                    searchWords={analysedData.message.mainSentences.map(value => value.value)}
+                                    autoEscape={true}
+                                    textToHighlight={changedData}
+                                />
+                        }
+
                     </CCardBody>
                 </CCollapse>
-
-                <CCardFooter></CCardFooter>
+                <CCardFooter>
+                </CCardFooter>
             </CCard>
         </CFade>
     )
